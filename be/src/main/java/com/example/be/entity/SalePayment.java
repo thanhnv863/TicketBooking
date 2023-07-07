e49ef7bb524d89cfffd890ce69e3de0ba5c26641
thanhnv863 <thanh862003hhbg@gmail.com>
2023-07-07 10:37:37 +0700

    thêm entity salePayment

---
+++ be/src/main/java/com/example/be/entity/SalePayment.java
@@ -0,0 +1,44 @@
+package com.example.be.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Entity
+@Table(name = "sale_payment")
+public class SalePayment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "amount")
+    private BigDecimal amount;
+
+    @Column(name = "payment_time")
+    private Date paymentTime;
+
+    @Column(name = "description")
+    private String description;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_order", referencedColumnName = "id")
+    private Order order;
+}
