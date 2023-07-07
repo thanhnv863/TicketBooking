8b87992e99b1cdc1f898960a24d57f82705310e8
thanhnv863 <thanh862003hhbg@gmail.com>
2023-07-08 00:24:10 +0700

    Curd order, Add dateFomart, Config Jackson + pom

---
+++ be/src/main/java/com/example/be/util/JacksonConfiguration.java
@@ -0,0 +1,23 @@
+package com.example.be.util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.boot.jackson.JsonComponentModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+@Configuration
+public class JacksonConfiguration {
+
+    @Bean
+    public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
+        ObjectMapper objectMapper = builder.build();
+        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        objectMapper.registerModule(new JsonComponentModule());
+        return objectMapper;
+    }
+
+}
+
+
